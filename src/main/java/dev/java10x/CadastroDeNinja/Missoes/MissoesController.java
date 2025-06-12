bd98f0cad3afb77c43cee094b196b0a2c1eba9d1
Fabricio3916 <fabriciopassos3916@gmail.com>
2025-06-12 16:07:44 -0300

    feat: cria o mock do MissionController

--- src/main/java/dev/java10x/CadastroDeNinja/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinja/Missoes/MissoesController.java
@@ -1,10 +1,39 @@
 package dev.java10x.CadastroDeNinja.Missoes;
 
+//LOCALHOST:8080
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
-@RequestMapping
+@RequestMapping("missoes")
 public class MissoesController {
+
+    //Get -- Mandar requisição para mostrar as missoes
+    @GetMapping("/listar")
+    public String listarMissoes(){
+        return "Missoes listadas";
+    }
+
+    //Post -- Mandar uma requisição para criar uma missao
+
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Criado missão";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "Alterado a missao";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissao(){
+        return "Missao deletada";
+    }
+
+
+
+
+
 }
