ac0dafc47a1d6af1ec528bcac5db21878a4096b0
Fabricio3916 <fabriciopassos3916@gmail.com>
2025-06-15 01:42:53 -0300

    feat: inclui camadas de DTO e Mapper e implementa seu uso na rota de criar ninja

--- src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaService.java
@@ -10,13 +10,17 @@ public class NinjaService {
 
 
     private NinjaRepository ninjaRepository;
+    private NinjaMapper ninjaMapper;
 
-    public NinjaService(NinjaRepository ninjaRepository) {
+    public NinjaService(NinjaRepository ninjaRepository, NinjaMapper ninjaMapper) {
         this.ninjaRepository = ninjaRepository;
+        this.ninjaMapper = ninjaMapper;
     }
 
-    public NinjaModel criarNinja(NinjaModel ninja){
-        return ninjaRepository.save(ninja);
+    public NinjaDTO criarNinja(NinjaDTO ninjaDTO){
+        NinjaModel ninja = ninjaMapper.map(ninjaDTO);
+        ninja = ninjaRepository.save(ninja);
+        return ninjaMapper.map(ninja);
     }
 
     public List<NinjaModel> listarNinjas(){
