4e3295adb80853448c10b902d397d7ab4b8020a2
Fabricio3916 <fabriciopassos3916@gmail.com>
2025-06-17 18:01:02 -0300

    feat: adiciona a parte inicial da UI com thymeleaf e criação do NinjaControllerUi

---
+++ src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaControllerUi.java
@@ -0,0 +1,29 @@
+package dev.java10x.CadastroDeNinja.Ninjas;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/ninjas/ui")
+public class NinjaControllerUi {
+
+    private final NinjaService ninjaService;
+
+    public NinjaControllerUi(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
+
+    @GetMapping("/listar")
+    public String listarNinjas(Model model){
+        List<NinjaDTO> ninjas = ninjaService.listarNinjas();
+        model.addAttribute("ninjas", ninjas);
+        return "Ninjas Listados";
+    }
+
+}
